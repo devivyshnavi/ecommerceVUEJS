9f5fadffb21a2ca7a8e4a3eed5cdaf1a215565dc
unknown <vyshnavi.tippa@neosoftmail.com>
2022-02-10 14:05:32 +0530

    VUEJS

---
+++ src/components/Tracking.vue
@@ -0,0 +1,47 @@
+<template>
+  <section id="cart_items">
+    <div class="container">
+      <div class="shopper-informations">
+        <div class="row">
+          <div class="col-sm-5">
+            <div class="shopper-info">
+              <p>Track order</p>
+
+              <form>
+                <input
+                  type="text"
+                  placeholder="Email"
+                  readonly
+                  name="email"
+                  v-model="email"
+                />
+                <input
+                  type="text"
+                  placeholder="Track id"
+                  name="trackid"
+                  v-model="trackId"
+                  required
+                />
+                <router-link :to="`/track/${trackId}`"> Track</router-link>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Tracking",
+  data() {
+    return {
+      email: localStorage.getItem("uid"),
+      trackId: undefined,
+    };
+  },
+};
+</script>
+
+<style></style>
